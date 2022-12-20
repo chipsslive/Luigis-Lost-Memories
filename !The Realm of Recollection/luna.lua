4638daad26dac57166fb7b55d9df38eed5853c4d
Chipss <53913001+chipsslive@users.noreply.github.com>
2022-12-19 22:32:32 -0600

    added chuck's return service

--- !The Realm of Recollection/luna.lua
+++ !The Realm of Recollection/luna.lua
@@ -1,3 +1,5 @@
+local littleDialogue = require("littleDialogue")
+
 local head = Graphics.loadImageResolved("LuigiHead.png");
 
 local currentRotation = 0
@@ -12,6 +14,14 @@ local initialY = 620
 local x = 100
 local y = -10
 
+local launch = false
+local triggered = false
+local timer = 0
+
+local chuck
+littleDialogue.registerAnswer("chuckQuestion",{text = "Yeah!",chosenFunction = function() launch = true end})
+littleDialogue.registerAnswer("chuckQuestion",{text = "Not Yet!"})
+
 function onStart()
     player.powerup = 2
     sprite = Sprite.box{
@@ -20,6 +30,8 @@ function onStart()
         y = y,
         pivot = v,
     }
+
+    chuck = Layer.get("chuck")
 end
 
 function onTick()
@@ -29,6 +41,33 @@ function onTick()
     if y < -20 then
         y = initialY
         x = initialX
+    end
+
+    if launch then
+        timer = timer + 1
+        player.speedX = -200000
+        player.speedY = -2
+        if not triggered then
+            chuck:hide(true)
+            triggerEvent("Lock Controls")
+            triggered = true
+        end
+        if timer == 7 then
+            SFX.play(9)
+        end
+        if timer > 100 then
+            launch = false
+            triggered = false
+            timer = 0
+            triggerEvent("Unlock Controls")
+            chuck:show(true)
+        end
+    end
+end
+
+function onEvent(eventName)
+    if eventName == "Lock Controls" then
+        Effect.spawn(805,-158214,-160222)
     end
 end
 
